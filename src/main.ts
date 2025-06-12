b01707e8725bf2a3afb0724c8e1241e3517b07f0
Raflizar Deswandi Yahya <raflizardeswandiy24@gmail.com>
2025-06-12 16:59:18 +0700

    add dotenv

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ZodValidationPipe } from './common/pipes/zod.validation.pipe';
@@ -5,8 +6,13 @@ import { ResponseTransformInterceptor } from './common/interceptors/response.int
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
     app.useGlobalPipes(new ZodValidationPipe());
     app.useGlobalInterceptors(new ResponseTransformInterceptor());
-    await app.listen(process.env.PORT ?? 3000);
+
+    const port = process.env.PORT || 3000;
+    await app.listen(port);
+
+    console.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
